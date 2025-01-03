f7888e6eb1ebea92c23e9a41aa7a94f34ac14ba5
Sandesh Poudel <141824006+Sandesh-007@users.noreply.github.com>
2025-01-03 09:21:42 +0545

    Tower of Hanoi problem

---
+++ lab4e.py
@@ -0,0 +1,13 @@
+#Tower of Hanoi in Python
+
+def toh(n, src, dst, tmp):
+    if n == 1:
+        print("Move disc " + str(n) + " from " + src + " to " + dst)
+    else:
+        toh(n-1, src, tmp, dst) 
+        print("Move disc " + str(n) + " from " + src + " to " + dst)
+        toh(n-1, tmp, dst, src)  
+
+num = int(input("Enter number of disks: "))
+print("The sequence will be:")
+toh(num, 'A', 'C', 'B') 
